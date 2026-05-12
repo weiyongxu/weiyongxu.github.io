8c9ea35f053d4570e7fee56aecddf277dd8b776c
weiyongxu <subtlelord@gmail.com>
2026-05-12 11:15:10 +0000

    Deploying to gh-pages from @ weiyongxu/weiyongxu.github.io@19f6b34be9ac62455e4900d000c89292bcfa2f69 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "See the PDF version of my CV for the most up-to-date information. You can also find the source code of this CV on GitHub.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
